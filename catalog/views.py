e05988b263c8fca00805e2d42c08c16be0f2be67
gm-willo <gianmarco.ninno@willo.it>
2026-01-26 10:02:19 +0100

    Add basic session data to store and retrieve how many times the users have visited LocalLibrary

--- catalog/views.py
+++ catalog/views.py
@@ -20,6 +20,11 @@ def index(request):
     num_genres_filtered = Genre.objects.filter(name__icontains="fant").count()
     num_books_filtered = Book.objects.filter(title__icontains="ring").count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get("num_visits", 0)
+    num_visits += 1
+    request.session["num_visits"] = num_visits
+
     context = {
         "num_books": num_books,
         "num_instances": num_instances,
@@ -27,6 +32,7 @@ def index(request):
         "num_authors": num_authors,
         "num_genres_filtered": num_genres_filtered,
         "num_books_filtered": num_books_filtered,
+        "num_visits": num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
