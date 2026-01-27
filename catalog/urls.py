53df7acc41602e4a4326315b541fe3a44eebdc0a
gm-willo <gianmarco.ninno@willo.it>
2026-01-27 15:12:43 +0100

    Create forms so that librarians can renew books, and create, update, and delete authors/books

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,9 +4,23 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("books/", views.BookListView.as_view(), name="books"),
-    path("books/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
+    path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
     path("authors/", views.AuthorListView.as_view(), name="authors"),
-    path("authors/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
+    path("author/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
-    path("borrowed/", views.BorrowedBooksByUsersListView.as_view(), name="borrowed-books")
+    path(
+        "borrowed/", views.BorrowedBooksByUsersListView.as_view(), name="borrowed-books"
+    ),
+    # Add URLConf for librarian to renew a book.
+    path(
+        "book/<uuid:pk>/renew/", views.renew_book_librarian, name="renew-book-librarian"
+    ),
+    # Add URLConf to create, update, and delete authors.
+    path("author/create/", views.AuthorCreate.as_view(), name="author-create"),
+    path("author/<int:pk>/update/", views.AuthorUpdate.as_view(), name="author-update"),
+    path("author/<int:pk>/delete/", views.AuthorDelete.as_view(), name="author-delete"),
+    # Add URLConf to create, update, and delete books.
+    path("book/create/", views.BookCreate.as_view(), name="book-create"),
+    path("book/<int:pk>/update/", views.BookUpdate.as_view(), name="book-update"),
+    path("book/<int:pk>/delete/", views.BookDelete.as_view(), name="book-delete"),
 ]
